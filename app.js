f531b9776be221ff8b5379ef1799faf35d904a39
aidynimanov <aidyn.imanov@gmail.com>
2022-06-29 00:41:14 +0600

    Second changes: working with exports and layout/template creation for further incorporation

--- app.js
+++ app.js
@@ -1,32 +1,40 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const date = require(__dirname+"/date.js");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 let items = ["make project", "learn coding", "find a job"];
-
+let workItems = [];
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-  let today = new Date();
-
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
-
-  let day = today.toLocaleDateString("en-US", options);
-  res.render("list", { kindOfDay: day, items: items });
+  let day = date.getDate();
+  res.render("list", {listTitle: day, items: items });
 });
 
 app.post("/", function (req, res) {
   let item = req.body.formForListItem;
-  items.push(item);
-  res.redirect("/");
+  console.log(req.body.list)
+  if (req.body.list === "Worklist"){
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+
 });
+
+app.get("/work", (req,res)=>{
+  res.render("list", {listTitle: "Worklist", items: workItems});
+});
+
+app.get("/about", (req, res)=>{
+  console.log(res)
+res.render("about");
+})
 
 app.listen(3000, () => {
   console.log("Server started on port 3000");
